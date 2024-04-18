cf1ac86782797b6a092336934acf3a2660318919
Vincent Zerbib <contact@zelabstudio.com>
2024-04-18 12:12:03 +0200

    save wokr

---
+++ src/app/account/profil/page.js
@@ -0,0 +1,11 @@
+import TitleMain from "@/components/UI/TitleMain";
+
+const Page = () => {
+    return (
+        <div>
+            <TitleMain title="Profil" />
+        </div>
+    );
+}
+
+export default Page;
