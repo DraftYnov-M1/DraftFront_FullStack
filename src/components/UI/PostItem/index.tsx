31e5f45248903b831c705c3c1a4015ed6c4d2c16
KevOneRedOne <kevinalves@hotmail.fr>
2024-05-02 10:19:29 +0200

    Add 'add to Wishlist' button in PostItem component and update WishlistContext

--- src/components/UI/PostItem/index.tsx
+++ src/components/UI/PostItem/index.tsx
@@ -1,6 +1,9 @@
+'use client';  
 import Link from "next/link";
 import styles from "./index.module.scss";
 import { Article } from "@/interfaces";
+import { useContext } from "react";
+import WishlistContext from "@/context/WishlistContext";
 
 interface Ipros {
     article: Article,
@@ -9,8 +12,13 @@ interface Ipros {
 
 const Index = ({article, position}: Ipros) => {
 
+    const { addArticle } = useContext(WishlistContext);
+
     return (
         <div className={styles.item} key={article.id}>
+                <button onClick={() => addArticle(article)}>
+                    add to Wishlist
+                </button>
                 <h3>{(position + 1).toString().padStart(2, "0")}</h3>
             <div className={styles.content}>                
                     <p>{article.date}</p>
@@ -19,9 +27,9 @@ const Index = ({article, position}: Ipros) => {
             <div className={styles.link}>
                 <Link href={`/blog/${article.id}`}>
                     <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g clip-path="url(#clip0_4_80)">
-                            <path d="M31.1667 25.6667H38.5V18.3334" stroke="#2C3E50" stroke-width="2.75" stroke-linecap="round" strokeLinejoin="round" />
-                            <path d="M5.5 22C7.12617 24.354 10.0467 25.7272 12.8333 25.6667C15.62 25.7272 18.5405 24.354 20.1667 22C21.7928 19.646 24.7133 18.2728 27.5 18.3333C30.2867 18.2728 33 20.1667 34.8333 22L38.5 25.6667" stroke="#2C3E50" stroke-width="2.75" stroke-linecap="round" strokeLinejoin="round" />
+                        <g clipPath="url(#clip0_4_80)">
+                            <path d="M31.1667 25.6667H38.5V18.3334" stroke="#2C3E50" strokeWidth="2.75" stroke-linecap="round" strokeLinejoin="round" />
+                            <path d="M5.5 22C7.12617 24.354 10.0467 25.7272 12.8333 25.6667C15.62 25.7272 18.5405 24.354 20.1667 22C21.7928 19.646 24.7133 18.2728 27.5 18.3333C30.2867 18.2728 33 20.1667 34.8333 22L38.5 25.6667" stroke="#2C3E50" strokeWidth="2.75" stroke-linecap="round" strokeLinejoin="round" />
                         </g>
                         <defs>
                             <clipPath id="clip0_4_80">
--- src/context/WishlistContext.tsx
+++ src/context/WishlistContext.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { Article } from "@/interfaces/";
 
 interface ContextProps {
@@ -19,17 +19,35 @@ export default WishlistContext;
 export const WishlistContextProvider = ({ children }: ContextProps) => {
     const [articles, setArticles] = useState<Article[]>([]);
 
-    const addProduct = (article: Article) => {
+    //chercher dans le localstorage si on a des articles sur la key wishlist
+    // à faire dans le useEffect
+
+    useEffect(() => {
+        if (localStorage.getItem('wishlist')) {
+            setArticles(JSON.parse(localStorage.getItem('wishlist') || '[]'));
+        } else {
+            localStorage.setItem('wishlist', JSON.stringify(articles));
+        }
+
+
+    }, []);
+
+
+    const addArticle = (article: Article) => {
         setArticles([...articles, article]);
+        //rajouter le nouvel article dans le localstorage
+        localStorage.setItem('wishlist', JSON.stringify([...articles, article]));
     };
     
     const removeProduct = (article: Article) => {
         setArticles(articles.filter((item) => item.id !== article.id));
+        //supprimer l'article du localstorage
+        localStorage.setItem('wishlist', JSON.stringify(articles.filter((item) => item.id !== article.id)));
     };
 
     const context = {
         articles,
-        addProduct,
+        addArticle,
         removeProduct,
     };
 
@@ -37,5 +55,5 @@ export const WishlistContextProvider = ({ children }: ContextProps) => {
         <WishlistContext.Provider value= { context } >
             { children }
         </WishlistContext.Provider>
-		);
+	);
 }
